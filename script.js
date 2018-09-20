2e4c776693496963c8008b5b6c4bd5e6823d0a98
Ethan-liao <ethan-liao@outlook.com>
2018-09-20 01:45:26 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", e => {
+  
+  
+  
+  
+  
+  
   document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 })
